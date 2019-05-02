6d3bd301c53270aeb18dcb0a5ab20a9f611de089
Tianhao Qiu <t6qiu@ucsd.edu>
2019-05-02 11:19:27 -0700

    Add Class 5

---
+++ class05/class05.R
@@ -0,0 +1,31 @@
+#' ---
+#' title: "Class 5: Making Graphs"
+#' author: "Tianhao Qiu"
+#' date: "April 16th, 2019"
+#' ---
+
+
+# Class 5 R graphics
+
+# 2a. Line plot
+weight <- read.table("bimm143_05_rstats/weight_chart.txt", header = TRUE)
+
+plot (weight$Age, weight$Weight, pch = 15, cex = 1.5, lwd = 2, ylim = c(2,10),
+      main = "Age vs Weight", xlab = "Age (months)", ylab = "Weight (kg)", typ = "o")
+
+# 2b. Bar Plot
+fcount <- read.table("bimm143_05_rstats/feature_counts.txt", header = TRUE, sep = "\t")
+par(mar = c(5, 10, 4.1, 2))
+barplot(fcount$Count, horiz = TRUE, ylab = " ", names.arg = fcount$Feature,
+        main = "Number of features in the mouse GRCm38 genome", las = 1, 
+        xlim = c(0, 80000))
+
+# 3a. Color Vectors
+# Optional (read.delim)
+fmcount <-read.table("bimm143_05_rstats/male_female_counts.txt", header = TRUE, sep = "\t")
+
+barplot(fmcount$Count, ylim = c(0,16), ylab = 'Counts', las = 2, 
+        names.arg = fmcount$Sample, col = rainbow(10))
+
+barplot(fmcount$Count, ylim = c(0,16), ylab = 'Counts', las = 2, 
+        names.arg = fmcount$Sample, col = c("red","blue", "green"))
